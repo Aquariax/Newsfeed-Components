1a424210279b30d4758a5ab7805c4a6912302dc5
Aquariax <roberthrrs8@gmail.com>
2019-03-06 20:07:34 -0500

    part of second stretch attempted

--- Article/Article.js
+++ Article/Article.js
@@ -7,22 +7,24 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton');    
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand"
+    this.expandButton.textContent = "Click to Expand"
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.deleteButton = domElement.querySelector('.delete');
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.expandButton.addEventListener('click', this.expandArticle.bind(this));
     this.deleteButton.addEventListener('click', () => this.deleteArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+    this.expandButton.textContent = "Click to Close";
   }
   deleteArticle(){
     this.domElement.style.display = "none";
     alert("article will be deleted!")
   }
 }
+
 
 /* START HERE: 
 
